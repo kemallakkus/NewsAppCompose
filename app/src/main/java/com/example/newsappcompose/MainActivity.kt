2ea42487c08da26277fb8a90d323e3f03d633cc3
kemallakkus <kemalllakkus@gmail.com>
2024-02-17 17:21:25 +0300

    create onboarding view model

--- app/src/main/java/com/example/newsappcompose/MainActivity.kt
+++ app/src/main/java/com/example/newsappcompose/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.newsappcompose
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -10,31 +9,23 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
-import androidx.lifecycle.lifecycleScope
-import com.example.newsappcompose.domain.usecases.AppEntryUseCases
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.newsappcompose.presentation.onboarding.OnboardingScreen
+import com.example.newsappcompose.presentation.onboarding.OnboardingViewModel
 import com.example.newsappcompose.ui.theme.NewsAppComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @Inject
-    lateinit var appEntryUseCases: AppEntryUseCases
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         installSplashScreen()
-        lifecycleScope.launch {
-            appEntryUseCases.readAppEntry().collect {
-                Log.d("Test", it.toString())
-            }
-        }
         setContent {
             NewsAppComposeTheme {
                 Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
-                    OnboardingScreen()
+                    val viewModel: OnboardingViewModel = hiltViewModel()
+                    OnboardingScreen(onEvent = viewModel::onEvent)
                 }
             }
         }

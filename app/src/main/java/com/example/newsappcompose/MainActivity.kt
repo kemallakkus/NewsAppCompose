c98a366344b4cba964e188a0feb4c1e6b46e594b
kemallakkus <kemalllakkus@gmail.com>
2024-02-17 18:55:36 +0300

    build navgraph

--- app/src/main/java/com/example/newsappcompose/MainActivity.kt
+++ app/src/main/java/com/example/newsappcompose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.newsappcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
@@ -10,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.newsappcompose.presentation.navgraph.NavGraph
 import com.example.newsappcompose.presentation.onboarding.OnboardingScreen
 import com.example.newsappcompose.presentation.onboarding.OnboardingViewModel
 import com.example.newsappcompose.ui.theme.NewsAppComposeTheme
@@ -17,15 +19,20 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        installSplashScreen()
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.splashCondition
+            }
+        }
         setContent {
             NewsAppComposeTheme {
                 Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
-                    val viewModel: OnboardingViewModel = hiltViewModel()
-                    OnboardingScreen(onEvent = viewModel::onEvent)
+                    val startDestination = viewModel.startDestination
+                    NavGraph(startDestination = startDestination)
                 }
             }
         }

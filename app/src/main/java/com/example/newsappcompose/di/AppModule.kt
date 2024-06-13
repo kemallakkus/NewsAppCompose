1824837f6ef08d21b6c008563b71049ee6a05f17
kemallakkus <kemalllakkus@gmail.com>
2024-06-13 13:34:43 +0300

    fix bug

--- app/src/main/java/com/example/newsappcompose/di/AppModule.kt
+++ app/src/main/java/com/example/newsappcompose/di/AppModule.kt
@@ -36,7 +36,7 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideLocalUserMager(
+    fun provideLocalUserManger(
         application: Application
     ): LocalUserManger = LocalUserMangerImpl(application)
 
